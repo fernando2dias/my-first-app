c3ed637bd0bcec475bd98bc23d524f92d2f44e25
Fernando Dias Motta <fernando3dias@gmail.com>
2023-12-06 16:27:52 -0300

    init

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer:boolean = false;
+  serverCreationStatus = "No server was created!";
+  serverName = '';
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit(){
+
+  }
+
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+
+}
