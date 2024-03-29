85c2d7103c0fc4cd7dda5ad303e25899631b910f
Fernando Dias Motta <fernando.motta@prilltc.onmicrosoft.com>
2024-03-29 18:04:25 -0300

    init new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './utils/warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './utils/success-alert/success-alert.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
